999488d95e942f118aa72abb4849fa54ded9815c
dtduy23 <146944393+dtduy23@users.noreply.github.com>
2024-04-04 02:54:34 +0700

    Update matching.cpp

--- matching.cpp
+++ matching.cpp
@@ -2,28 +2,28 @@
 
 // Ki?m tra I matching
 bool checkI(POKEMON** content, int x1, int y1, int x2, int y2) {
-	// Hai ô không th? gi?ng nhau
+	// Hai Ã´ khÃ´ng th? gi?ng nhau
 	if (x1 == x2 && y1 == y2) return false;
 
-	// Ki?m tra hai ô có gi?ng nhau không
+	// Ki?m tra hai Ã´ cÃ³ gi?ng nhau khÃ´ng
 	if ((abs(x1 - x2) == 1 && y1 == y2) || (x1 == x2 && abs(y1 - y2) == 1)) return true;
 
-	// Tr??ng h?p hai ô n?m trên cùng m?t hàng
+	// Tr??ng h?p hai Ã´ n?m trÃªn cÃ¹ng m?t hÃ ng
 	if (y1 == y2) {
 		int startX = min(x1, x2) + 1;
 		int endX = max(x1, x2);
 		for (int i = startX; i < endX; ++i) {
-			if (content[i][y1].data != 0) return false; // Ki?m tra xem có ô tr?ng n?m gi?a chúng không
+			if (content[i][y1].data != 0) return false; // Ki?m tra xem cÃ³ Ã´ tr?ng n?m gi?a chÃºng khÃ´ng
 		}
 		return true;
 	}
 
-	// Tr??ng h?p hai ô n?m trên cùng m?t c?t
+	// Tr??ng h?p hai Ã´ n?m trÃªn cÃ¹ng m?t c?t
 	if (x1 == x2) {
 		int startY = min(y1, y2) + 1;
 		int endY = max(y1, y2);
 		for (int i = startY; i < endY; ++i) {
-			if (content[x1][i].data != 0) return false; // Ki?m tra xem có ô tr?ng n?m gi?a chúng không
+			if (content[x1][i].data != 0) return false; // Ki?m tra xem cÃ³ Ã´ tr?ng n?m gi?a chÃºng khÃ´ng
 		}
 		return true;
 	}
@@ -33,13 +33,13 @@ bool checkI(POKEMON** content, int x1, int y1, int x2, int y2) {
 // Ki?m tra L matching
 bool checkL(POKEMON** content, int x1, int y1, int x2, int y2)
 {
-	// Hai ô không th? t?o L matching
+	// Hai Ã´ khÃ´ng th? t?o L matching
 	if (x1 == x2 && y1 == y2) return false;
 
-	// Ki?m tra xem có th? t?o L matching t? hai ô ???c ch?n
-	if (checkI(content, x1, y1, x2, y1) && checkI(content, x2, y2, x2, y1) && content[x1][y2].data == 0)
+	// Ki?m tra xem cÃ³ th? t?o L matching t? hai Ã´ ???c ch?n
+	if (checkI(content, x1, y1, x2, y1) && checkI(content, x2, y2, x2, y1) && content[x2][y1].data == 0)
 		return true;
-	if (checkI(content, x1, y1, x1, y2) && checkI(content, x2, y2, x1, y2) && content[x2][y1].data == 0)
+	if (checkI(content, x1, y1, x1, y2) && checkI(content, x2, y2, x1, y2) && content[x1][y2].data == 0)
 		return true;
 
 	return false;
@@ -47,10 +47,10 @@ bool checkL(POKEMON** content, int x1, int y1, int x2, int y2)
 // Ki?m tra Z matching
 bool checkZ(POKEMON** content, int x1, int y1, int x2, int y2)
 {
-	// Ki?m tra hai ô không trùng nhau
+	// Ki?m tra hai Ã´ khÃ´ng trÃ¹ng nhau
 	if (x1 == x2 && y1 == y2) return false;
 
-	// Ki?m tra xiên xu?ng
+	// Ki?m tra xiÃªn xu?ng
 	/*
 	  x
 		x
@@ -72,7 +72,7 @@ bool checkZ(POKEMON** content, int x1, int y1, int x2, int y2)
 				return true;
 		}
 	}
-	// Ki?m tra xiên lên
+	// Ki?m tra xiÃªn lÃªn
 	/*
 		  x
 		x
